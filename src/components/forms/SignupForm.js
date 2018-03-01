acd44e591e44e44a4231698dbd536e9b39123a25
Thach Pham <thach@dintero.com>
2018-03-01 14:42:28 +0100

    fix bug in signup form

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -18,11 +18,11 @@ class SignupForm extends React.Component {
         data: {...this.state.data, [e.target.name]: e.target.value}
     });
 
-    onSubmit = e => {
+    onSubmit = (e) => {
         e.preventDefault();
         const errors = this.validate(this.state.data);
         this.setState({errors});
-        if (Object.keys(errors) === 0) {
+        if (Object.keys(errors).length === 0) {
             this.setState({loading: true})
 
             this.props.submit(this.state.data)
@@ -41,29 +41,30 @@ class SignupForm extends React.Component {
     }
 
     render() {
-        const { data , errors, loading} = this.state;
+        const { data, errors, loading } = this.state;
         return (
             <Form onSubmit={this.onSubmit} loading={loading}>
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="email@email.com"
-                        value={ data.email }
-                        onChange= {this.onChange}
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    placeholder="example@gmail.com"
+                    value= { data.email}
+                    onChange={ this.onChange } 
                     />
                     {errors.email && <InlineError text={errors.email}/>}
                 </Form.Field>
                 <Form.Field error={!!errors.password}>
                     <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={ data.password }
-                        onChange={ this.onChange }
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Make it sercure"
+                    value= { data.password}
+                    onChange={ this.onChange } 
                     />
                     {errors.password && <InlineError text={errors.password}/>}
                 </Form.Field>
